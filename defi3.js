eac9b9ecdea0fd872e3816385a6eee93f9316062
YoulaMamadouba <youlamamadouba240@gmail.com>
2024-08-08 13:52:57 +0200

    Ajout du fichier defi3.js

---
+++ defi3.js
@@ -0,0 +1,13 @@
+// let's go 
+
+let age=17;
+let total =100;
+
+let result = age >= 21 ? " le boisson offert pour cet age est le wiski ou du vin " :  age >=18 && age<=20   ?"le boisson offert pour cet age est le vin": "du jus de fruits pour vous";
+console.log(result);
+
+let remise= total>=100 ? "vous obtenez une remise de 20%" : total>=50 ? " vous obtenez une remise de 10%" : " nothing to you " 
+
+console.log(remise)
+
+
